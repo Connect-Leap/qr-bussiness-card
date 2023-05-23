dbb195e05243ae830cf366ef9929b68ac41d47c5
dayCod <alayarahman07@gmail.com>
2023-05-23 15:12:29 +0700

    Update: Show all users with statistic

---
+++ app/Http/Controllers/Backend/Admin/MasterUser/UsersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Admin\MasterUser;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function seeAllUsers()
+    {
+        $roles = ['employee', 'supervisor'];
+
+        $users = User::whereIn('role', $roles)->latest()->get();
+        $user_total = $users->count();
+        $user_supervisor_total = User::where('role', 'supervisor')->count();
+        $user_employee_total = User::where('role', 'employee')->count();
+
+        return view('pages.master-user.detail-users.index', [
+            'users' => $users,
+            'user_total' => $user_total,
+            'user_supervisor_total' => $user_supervisor_total,
+            'user_employee_total' => $user_employee_total
+        ]);
+    }
+}
---
+++ resources/views/pages/master-user/detail-users/index.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Show All Users'])
+    <div class="row mt-4 mx-4">
+        <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Users</p>
+                                <h5 class="font-weight-bolder">
+                                    {{ $user_total }}
+                                </h5>
+                                <p class="mb-0">
+                                    Since
+                                    <span class="text-success text-sm font-weight-bolder">{{ now() }}</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-info shadow-info text-center rounded-circle">
+                                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Supervisor</p>
+                                <h5 class="font-weight-bolder">
+                                    {{ $user_supervisor_total }}
+                                </h5>
+                                <p class="mb-0">
+                                    Since
+                                    <span class="text-success text-sm font-weight-bolder">{{ now() }}</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-info shadow-info text-center rounded-circle">
+                                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Employee</p>
+                                <h5 class="font-weight-bolder">
+                                    {{ $user_employee_total }}
+                                </h5>
+                                <p class="mb-0">
+                                    Since
+                                    <span class="text-success text-sm font-weight-bolder">{{ now() }}</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-info shadow-info text-center rounded-circle">
+                                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4 mx-4">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="card-header d-flex align-items-start justify-content-between pb-0">
+                    <h6>Show All Users</h6>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        #
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Email
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Position
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Role
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse($users as $user)
+                                    <tr>
+                                        <td class="text-center">
+                                            #
+                                        </td>
+                                        <td class="text-center">
+                                            <p class="text-sm font-weight-bold mb-0">{{ $user->email }}</p>
+                                        </td>
+                                        <td class="text-center">
+                                            <p class="text-sm font-weight-bold mb-0">{{ $user->position->name }}
+                                            </p>
+                                        </td>
+                                        <td class="text-center">
+                                            <p class="text-sm font-weight-bold mb-0">{{ ucfirst($user->role) }}
+                                            </p>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="7" class="text-center">Empty Data</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
