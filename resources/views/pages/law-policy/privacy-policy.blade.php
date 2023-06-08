f8c6e72dd6a07e692a0e120d33f73fc2f52b3f41
dayCod <alayarahman07@gmail.com>
2023-06-08 16:29:01 +0700

    Update: Database (Not Yet) and Policy Document

---
+++ resources/views/pages/law-policy/privacy-policy.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connect Leap | Privacy Policy</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Source Code Pro', monospace;
+            margin: 80px 100px;
+            font-weight: 600;
+        }
+    </style>
+</head>
+
+<body>
+    {{-- Title --}}
+    <h2>Connect Leap | Privacy Policy</h2>
+
+    {{-- Main --}}
+    <p>This Privacy Policy describes how Connect Leap collects, uses, and protects the personal information of users of our website, mobile application, or any other services we provide (collectively referred to as the "Services"). By accessing or using our Services, you acknowledge and accept the practices described in this Policy.</p>
+
+    {{-- Information Collection --}}
+    <h3>1. Information Collection and Use</h3>
+    <p>We may collect personal information, such as your name, contact details, and other relevant data when you voluntarily provide it to us for the purpose of accessing or using our Services.</p>
+
+    {{-- Use of Collected Information --}}
+    <h3>2. Use of Collected Information</h3>
+    <p>We may use the collected information for the following purposes:</p>
+    <ul>
+        <li>Provide, maintain, and improve our Services</li>
+        <li>Respond to inquiries or requests from Users</li>
+        <li>Personalize and enhance User experience</li>
+        <li>Communicate with Users about updates, promotions, or relevant information</li>
+        <li>Comply with legal obligations</li>
+    </ul>
+
+    {{-- Information Sharing --}}
+    <h3>3. Information Sharing</h3>
+    <p>We may share your personal information with trusted third-party service providers who assist us in operating our Services or conducting our business activities. We do not sell, trade, or rent your personal information to third parties for marketing purposes.</p>
+
+    {{-- Data Security --}}
+    <h3>4. Data Security</h3>
+    <p>We implement reasonable security measures to protect the personal information we collect and store. However, no data transmission over the internet or electronic storage method can guarantee absolute security. We encourage Users to take appropriate steps to protect their personal information as well.</p>
+
+    {{-- Cookies and Tracking Technologies --}}
+    <h3>5. Cookies and Tracking Technologies</h3>
+    <p>We may use cookies or similar tracking technologies to enhance User experience, gather usage information, and improve our Services. Users can adjust their browser settings to refuse cookies or receive notifications when cookies are being sent.</p>
+
+    {{-- Third-Party Links --}}
+    <h3>6. Third-Party Links</h3>
+    <p>Our Services may contain links to third-party websites or services. We are not responsible for the privacy practices or content of such third parties. Users should review the privacy policies of those third parties before providing any personal information.</p>
+
+    {{-- Children's Privacy --}}
+    <h3>7. Children's Privacy</h3>
+    <p>Our Services are not intended for children under the age of 13. We do not knowingly collect personal information from children. If you believe we may have collected information from a child, please contact us to have it removed.</p>
+
+    {{-- Updates to the Privacy Policy --}}
+    <h3>8. Updates to the Privacy Policy</h3>
+    <p>We reserve the right to update or modify this Privacy Policy at any time. Any changes will be effective upon posting the revised Policy on our website. It is your responsibility to review this Policy periodically for any updates.</p>
+
+    {{-- Contact Us --}}
+    <h3>9. Contact Us</h3>
+    <p>If you have any questions, concerns, or requests regarding this Privacy Policy or the handling of your personal information, please contact us at <a href="mailto:connectleapbusdev@gmail.com">connectleapbusdev@gmail.com</a></p>
+</body>
+
+</html>
---
+++ resources/views/pages/law-policy/terms-and-agreement.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connect Leap | Privacy Policy</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Source Code Pro', monospace;
+            margin: 80px 100px;
+            font-weight: 600;
+        }
+    </style>
+</head>
+
+<body>
+    {{-- Title --}}
+    <h2>Connect Leap | License and Agreement</h2>
+
+    {{-- Main --}}
+    <p>This License and Agreement is entered into between Connect Leap as “Licensor” and You “Licensee" (collectively referred to as the "Parties") on [Date].</p>
+
+    {{-- License Grant --}}
+    <h3>1. License Grant</h3>
+    <p>Licensor hereby grants Licensee a non-exclusive, non-transferable license to use [Product/Service] ("Licensed Materials") in accordance with the terms and conditions of this Agreement.</p>
+
+    {{-- Scope of Use --}}
+    <h3>2. Scope of Use</h3>
+    <p>Licensee may use the Licensed Materials solely for [specified purpose] and within the territory of [specified territory].</p>
+
+    {{-- Intellectual Property --}}
+    <h3>3. Intellectual Property</h3>
+    <p>Licensor retains all rights, title, and interest in and to the Licensed Materials, including any copyrights, trademarks, or other intellectual property rights associated with them.</p>
+
+    {{-- Payment and Fees --}}
+    <h3>4. Payment and Fees</h3>
+    <p>Licensee shall pay Licensor the agreed-upon fees for the use of the Licensed Materials, as outlined in Exhibit A attached to this Agreement.</p>
+
+    {{-- Term and Termination --}}
+    <h3>5. Term and Termination</h3>
+    <p>This Agreement shall commence on the effective date and remain in effect until terminated by either party. Either party may terminate this Agreement upon written notice if the other party breaches any material term or condition.</p>
+
+    {{-- Confidentiality Licensee --}}
+    <h3>6. Confidentiality Licensee</h3>
+    <p>Agrees to keep any confidential information obtained from Licensor during the term of this Agreement confidential and not disclose it to any third parties.</p>
+
+    {{-- Limitation of Liability --}}
+    <h3>7. Limitation of Liability</h3>
+    <p>In no event shall either party be liable to the other for any indirect, consequential, incidental, or punitive damages arising out of or related to this Agreement.</p>
+
+    {{-- Governing Law and Jurisdiction --}}
+    <h3>8. Governing Law and Jurisdiction</h3>
+    <p>This Agreement shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any disputes arising from this Agreement shall be subject to the exclusive jurisdiction of the courts of [Jurisdiction].</p>
+
+    {{-- Entire Agreement --}}
+    <h3>9. Entire Agreement</h3>
+    <p>This Agreement constitutes the entire understanding between the Parties and supersedes any prior agreements or understandings, whether written or oral, relating to the subject matter herein.</p>
+</body>
+
+</html>
