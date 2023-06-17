d75a039112afe3103bb87234c53c2fbaa40d35b1
dayCod <alayarahman07@gmail.com>
2023-06-17 14:23:11 +0700

    Update: QrController::class to short url route

--- routes/short-url.php
+++ routes/short-url.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Backend\Admin\QR\QrController;
+
 Route::get('/short/{urlkey}/{qr_id}', [QrController::class, 'QrProcessing'])->name('master-qr.qr-processing');
 Route::get('/short/{qr_id}/vcard/process', [QrController::class, 'QrVcardProcessing'])->name('master-qr.qr-vcard-processing');
