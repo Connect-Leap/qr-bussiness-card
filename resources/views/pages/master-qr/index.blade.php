ea048b51c4e6701887f8947daa57a5f975eefe66
dayCod <alayarahman07@gmail.com>
2023-06-17 14:08:26 +0700

    Update: deactivate download

--- resources/views/pages/master-qr/index.blade.php
+++ resources/views/pages/master-qr/index.blade.php
@@ -82,7 +82,7 @@
                                                                 @endif
                                                             </div>
                                                             <div class="modal-footer">
-                                                                <a class="btn btn-primary" href="{{ $qrcode['qrcode']['file_storage']['image_file'][0]['file_url'] }}" download>Download QR</a>
+                                                                <a class="btn btn-primary" href="{{ $qrcode['qrcode']['file_storage']['image_file'][0]['file_url'] }}">Download QR</a>
                                                                 <button type="button" class="btn btn-danger"
                                                                     data-bs-dismiss="modal">Close</button>
                                                             </div>
