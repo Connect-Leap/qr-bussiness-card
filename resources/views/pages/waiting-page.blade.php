13ae719d56c227e240b843f3009dfa225d9de0cf
dayCod <alayarahman07@gmail.com>
2023-06-24 13:33:58 +0700

    Update: Loading Page Ui

--- resources/views/pages/waiting-page.blade.php
+++ resources/views/pages/waiting-page.blade.php
@@ -9,15 +9,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Gothic&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('libraries/fakeLoader.js/dist/fakeLoader.min.css') }}">
 
     <title>Wait for a moment...</title>
+    <style>
+        body { font-family: 'League Gothic', sans-serif; background-color: #08428C; }
+    </style>
 </head>
 
 <body>
     <main>
-
         <section id="loader">
             <div class="fakeLoader"></div>
         </section>
@@ -25,12 +30,20 @@
         <section id="afterLoad" class="d-none">
             <div class="row justify-content-center align-items-center" style="height: 95vh">
                 <div class="col-12">
-                    <h5 class="text-center">Open using Recomended System Contact </h5>
-                    <div class="text-center mt-4">
-                        <a href="{{ $destination }}" class="btn btn-primary">Download Contact File Here</a>
+                    <div class="text-center">
+                        <img src="{{ asset('img/logos/connect-leap-logo.png') }}" class="img-fluid" width="120" alt="">
+                    </div>
+                    <div class="text-center my-3">
+                        <h1 class="text-uppercase text-white">connect leap</h1>
+                    </div>
+                    <div class="text-center mb-3">
+                        <h3 class="text-uppercase text-white">imprting contact ...</h3>
+                    </div>
+                    <div class="text-center mb-3">
+                        <p class="text-uppercase text-xl text-white">please click <a href="{{ $destination }}" class="text-white fw-bold" download>download</a> if there is no download</p>
                     </div>
                     <div class="text-center">
-                        <sup class="text-danger text-xs">Click button above if Popup not showed up</sup>
+                        <p class="text-uppercase text-white text-lg">you can check the contact at your phone by home > contacts > search contact</p>
                     </div>
                 </div>
             </div>
