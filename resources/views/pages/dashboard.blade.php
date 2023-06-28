5d5664c3e4524d203810d41ca6c30b8cfee94db7
dayCod <alayarahman07@gmail.com>
2023-06-28 18:45:45 +0700

    Update: Remove Row 1 Dashboard for a while

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('layouts.navbars.auth.topnav', ['title' => 'Dashboard'])
     <div class="container-fluid py-4">
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
                     <div class="card-body p-3">
@@ -103,7 +103,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <div class="row mt-4">
             {{-- <div class="col-lg-7 mb-lg-0 mb-4">
                 <div class="card z-index-2 h-100">
