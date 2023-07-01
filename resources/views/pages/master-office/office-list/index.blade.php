bda43aaa4230248b748371e765a9adb728e9e600
dayCod <alayarahman07@gmail.com>
2023-07-01 10:49:37 +0700

    Update: Copywriting - Office & Sidenav

--- resources/views/pages/master-office/office-list/index.blade.php
+++ resources/views/pages/master-office/office-list/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Management Office'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Office Detail'])
     <div class="row mt-4 mx-4">
         <div class="col-12">
             <div class="card mb-4">
                 <div class="card-header d-flex align-items-start justify-content-between pb-0 mb-3">
-                    <h6>Management Office</h6>
+                    <h6>Office Detail</h6>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="container">
