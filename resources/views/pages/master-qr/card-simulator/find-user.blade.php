d9a2de988c820d8306b8a459d08b29f5f56f4cb1
dayCod <alayarahman07@gmail.com>
2023-07-01 10:41:42 +0700

    Update: Copywriting - Card Preview

--- resources/views/pages/master-qr/card-simulator/find-user.blade.php
+++ resources/views/pages/master-qr/card-simulator/find-user.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Create your Card Simulator'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Create your Card Preview'])
     <div class="row justify-content-center mt-5 mx-4">
         <div class="col-8">
             <div class="card mb-4">
                 <div class="card-header d-flex align-items-start justify-content-between pb-0">
-                    <h6>Find User - Card Simulator</h6>
+                    <h6>Find User - Card Preview</h6>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="container">
--- resources/views/pages/master-qr/card-simulator/view-card.blade.php
+++ resources/views/pages/master-qr/card-simulator/view-card.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Create your Card Simulator'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Card Preview'])
     <div class="row justify-content-center mt-5 mx-4">
         <div class="col-8">
             <div class="card mb-4">
                 <div class="card-header d-flex align-items-start justify-content-between pb-0">
-                    <h6>Card Simulator</h6>
+                    <h6>Card Preview</h6>
                     <a href="{{ route('card-simulator.index') }}" class="btn btn-info btn-sm">Back</a>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
